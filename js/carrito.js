617c59608ab495d4bb04060de0d685be8cafbe9f
Geider10 <geiderde20@gmail.com>
2024-01-20 23:31:38 -0300

    el array productos guardan las cartas del dom y luego se cargan las cartas seleccionadas en el carrito

--- js/carrito.js
+++ js/carrito.js
@@ -4,9 +4,35 @@ const btnCloseCart = d.getElementById("btnCloseCart");
 //arrays
 const todoProductos = d.querySelectorAll(".card2")
 let carritoProductos = [];
-
+let conta = 0;
+//cards
+const contentModal = d.getElementById("contentModal");
+todoProductos.forEach(p =>{
+    const btnComprar = p.children[1].children[1];
+    btnComprar.addEventListener("click",()=>{
+        carritoProductos.push({
+            Id: conta,
+            Img: p.children[0].children[0].getAttribute("src"),
+            Name: p.children[0].children[1].textContent,
+            Price: parseInt(p.children[1].children[0].textContent)
+        })
+        conta++;
+    })
+})
 btnCart.addEventListener("click",(e)=>{
+    contentModal.innerHTML="";
     d.getElementById("conteinerModal").classList.remove("cOff");
+    carritoProductos.forEach(p =>{
+        const cartProduct = d.createElement("div");
+        cartProduct.classList.add("cartProduct");
+        cartProduct.innerHTML=`
+        <img src="${p.Img}" class="imgModal" alt="Sapatilla con estilo">
+        <h3>${p.Name}</h3>
+        <p>${p.Price}</p>
+        <button class="closeProduct" id="btnCloseProduct"><i class="fa-solid fa-trash-can"></i></button>
+        `;
+        contentModal.appendChild(cartProduct);
+    })
 })
 btnCloseCart.addEventListener("click",()=>{
     d.getElementById("conteinerModal").classList.add("cOff");
