da43723f83b4461e90377206eadc7ed931200738
Geider10 <geiderde20@gmail.com>
2024-01-25 23:05:20 -0300

    se actualiza la cantidad y el precio de los productos repetidos

--- js/carrito.js
+++ js/carrito.js
@@ -1,38 +1,44 @@
 const d = document;
 const btnCart = d.getElementById("btnCart");
 const btnCloseCart = d.getElementById("btnCloseCart");
-//arrays
 const todoProductos = d.querySelectorAll(".card2")
 let carritoProductos = [];
 let conta = 0;
-let seRepite = false;
+
 //llenar carrito con productos seleccionados
 const contentModal = d.getElementById("contentModal");
 todoProductos.forEach(p =>{
     const btnComprar = p.children[1].children[1];
-    btnComprar.addEventListener("click",()=>{
-        const pId = 2;
-        // console.log(pId);
-        carritoProductos.forEach(p => {
-            if(p.Id === pId){
-                seRepite = true;
-                console.log(seRepite);
+    btnComprar.addEventListener("click",(e)=>{
+        const nam = p.children[0].children[1].textContent;
+        const ckeck = carritoProductos.some(product =>{
+            if(product.Name === nam){
+                return true;
             }
             else{
-                console.log(seRepite);
+                return false;
             }
-        });
-        carritoProductos.push({
-            Id: conta,
-            Img: p.children[0].children[0].getAttribute("src"),
-            Name: p.children[0].children[1].textContent,
-            Price: parseInt(p.children[1].children[0].textContent),
-            Quantity : 1
-        });
-        conta++;
-        countCart();
+        })
+        if(ckeck){
+            carritoProductos.map((p)=>{
+                if(p.Name == nam)
+                p.Quantity++;
+            });
+        }
+        else{
+            carritoProductos.push({
+                Id: conta,
+                Img: p.children[0].children[0].getAttribute("src"),
+                Name: p.children[0].children[1].textContent,
+                Price: parseInt(p.children[1].children[0].textContent),
+                Quantity : 1
+            });
+            conta++;
+            countCart();
+        }
     })
 })
+
 btnCart.addEventListener("click",(e)=>{
     contentModal.innerHTML="";
     d.getElementById("conteinerModal").classList.remove("cOff");
@@ -42,16 +48,16 @@ btnCart.addEventListener("click",(e)=>{
         cartProduct.innerHTML=`
         <img src="${p.Img}" class="imgModal" alt="Sapatilla con estilo">
         <h3>${p.Name}</h3>
-        <p>${p.Price}</p>
         <p>${p.Quantity}</p>
+        <p>${p.Price * p.Quantity}</p>
         <button class="closeProduct" id="btnCloseProduct">X</button>
+        <p class="cOff">${p.Id}</p>
         `;
         contentModal.appendChild(cartProduct);
         const btnDeleteProduct = cartProduct.children[4];
         deleteProduct(btnDeleteProduct,p);
     })
-    let totalPagar = carritoProductos.reduce((acc, p)=>acc + p.Price,0);
-    d.getElementById("payTotal").textContent = totalPagar;
+   refreshTotal();
 })
 btnCloseCart.addEventListener("click",()=>{
     d.getElementById("conteinerModal").classList.add("cOff");
@@ -67,6 +73,7 @@ const deleteProduct=(btnDelete,product)=>{
                 carritoProductos.splice(conta2,1);
                 conta2 = 0;
                 countCart();
+                refreshTotal();
             }
             else{
                 conta2++;
@@ -75,7 +82,6 @@ const deleteProduct=(btnDelete,product)=>{
     })
 }
 const countCart = ()=>{ 
-   
     if(carritoProductos.length >= 1){
         let long = carritoProductos.length;
         const quantityProducts = d.getElementById("countCart");
@@ -83,9 +89,10 @@ const countCart = ()=>{
         quantityProducts.innerText = long;
     }
     else{
-        // const quantityProducts = d.getElementById("countCart");
-        // quantityProducts.style.display = "none";
         d.getElementById("countCart").style.display ="none";
     }
-   
+}
+const refreshTotal = ()=>{
+    let totalPagar = carritoProductos.reduce((acc, p)=>acc + (p.Price * p.Quantity),0);
+    d.getElementById("payTotal").textContent = totalPagar;
 }
